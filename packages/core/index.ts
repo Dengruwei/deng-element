17cc6b80657383c1ce385484c0aa066e127a31d2
Dengruwei <1106992632@qq.com>
2024-10-07 15:49:02 +0800

    :tada: first commit

---
+++ packages/core/index.ts
@@ -0,0 +1,8 @@
+import {makeInstaller} from '@toy-element/utils'
+import components from './components'
+import '@toy-element/theme'
+
+const installer = makeInstaller(components)
+
+export * from "@toy-element/components"
+export default installer
